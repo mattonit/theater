9747111efbfba89a1d8ccaf5b851571be18cbe33
Mattonit <bartlomiej.malanowski@hotmail.com>
2018-10-22 07:22:08 +0200

    added show option

--- js/theater.js
+++ js/theater.js
@@ -7,20 +7,95 @@
 
 (($) => {
 
+  const KEY_ESC   = 27;
+  const KEY_LEFT  = 37;
+  const KEY_RIGHT = 39;
+
   class Theater {
 
     constructor(element, options) {
 
-      this.defaults = {};
+      this.keyCodes = {
+        esc: KEY_ESC,
+        left: KEY_LEFT,
+        right: KEY_RIGHT
+      };
+
+      this.$element = $(element);
+
+      this.$window = $('window');
+      this.$body = $('body');
+      this.$overlay = $('<div id="theater-overlay"></div>');
+      this.$container = $('<div id="theater-container"></div>');
+
+      this.src = $(this.$element[0]).attr('href');
+
+      this.defaults = {
+        loop: false,
+        keyboard: true,
+        arrows: true,
+        template:
+        `<div id="theater-overlay">
+          ${ this.renderMedia() }
+         </div>`,
+        slideshow: true,
+        closeOnClick: true,
+        overlay: true
+      };
+
+      this.options = this.assignOptions(options);
+
+      this.bindEvents();
+
+    }
+
+    assignOptions(newOptions) {
+
+      return $.extend({}, this.defaults, newOptions);
+    }
+
+    bindEvents() {
+
+      this.$element.on('click', this.show.bind(this));
+      this.$window.on('keyup', this.handleKeyboard.bind(this));
+
+    }
+
+    handleKeyboard() {
+      if(this.options.keyboard === false) {
+        return false;
+      }
+    }
+
+    renderMedia() {
+      return `<img src="${ this.src }" class="img-fluid" />`;
+
+    }
+
+    show(e) {
+
+      e.preventDefault();
+
+      if(this.options.overlay) {
+        this.$body.append(this.$overlay);
+      }
+
+      this.$body.append(this.$container);
+
+      this.$container.html(this.options.template);
+
+    }
+
+    close() {
     }
 
   }
 
   $.fn.theater = function (options) {
     return this.each(function () {
-      const theater = new Theater($(this), options);
-      return theater;
+      new Theater($(this), options);
     });
+
   };
 
 })(jQuery);
